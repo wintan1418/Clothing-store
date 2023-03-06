cf45579d373cfc6e34eab588cea069737648359b
wintan1418 <wintan1418@gmail.com>
2023-03-06 16:14:59 +0100

    Add categories for products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class = "categories-container"> 
+    <div class="category-container">
+      {/* <img/> */}
+      <div class="body-container">
+        <h2>Hat</h2>
+        <p>shop now </p>
+      </div>
     </div>
-  );
-}
+     <div class="category-container">
+      {/* <img/> */}
+      <div class="body-container">
+        <h2>Sneakers</h2>
+        <p>shop now </p>
+      </div>
+    </div>
+
+ <div class="category-container">
+      {/* <img/> */}
+      <div class="body-container">
+        <h2>Jacket</h2>
+        <p>shop now </p>
+      </div>
+    </div>
+
+ <div class="category-container">
+      {/* <img/> */}
+      <div class="body-container">
+        <h2>Watch</h2>
+        <p>shop now </p>
+      </div>
+    </div>
+
+ <div class="category-container">
+      {/* <img/> */}
+      <div class="body-container">
+        <h2>shirt</h2>
+        <p>shop now </p>
+      </div>
+    </div>
+
+
+    </div>
+  )  
+  
+};
 
 export default App;
